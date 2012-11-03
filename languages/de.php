d61d46d7b3c194e37beb75222b161a6e867c6af4
unknown <manactric@gmail.com>
2012-11-03 09:50:32 +0500

    German Language

---
+++ languages/de.php
@@ -0,0 +1,15 @@
+<?php
+/* Liang Lee Rightclick Plugin
+ * @website Link: http://community.elgg.org/pg/profile/arsalanlee/
+ * @package Liang Lee Rightclick Plugin
+ * @author Liang Lee
+ * @copyright All right reserved Liang Lee 2012.
+ * @File de.php German Language by Nudeler2 http://community.elgg.org/pg/profile/Nudeler2
+ */
+
+$german = array(
+	'llee:rc:1' => 'Geben Sie hier einen Text angezeigt werden, wenn Benutzer mit der rechten klicken Sie klicken Sie auf.',
+
+);
+
+add_translation('de', $german);
