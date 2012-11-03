8c63b0bd545acee169c6628f2f6a8f9f684c7ae0
unknown <manactric@gmail.com>
2012-11-03 09:50:43 +0500

    Englsih Language

---
+++ languages/en.php
@@ -0,0 +1,15 @@
+<?php
+/* Liang Lee Rightclick Plugin
+ * @website Link: http://community.elgg.org/pg/profile/arsalanlee/
+ * @package Liang Lee Rightclick Plugin
+ * @author Liang Lee
+ * @copyright All right reserved Liang Lee 2012.
+ * @File en.php 
+ */
+
+$english = array(
+	'llee:rc:1' => 'Please Enter here a text to be displayed when user click right click.',
+
+);
+
+add_translation('en', $english);
