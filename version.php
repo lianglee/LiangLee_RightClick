4f43046a787b31b4ae261d726f434cf3a92d7e0b
unknown <manactric@gmail.com>
2012-11-03 09:51:13 +0500

    version

---
+++ version.php
@@ -0,0 +1,12 @@
+<?php
+/* Liang Lee Rightclick Plugin
+ * @website Link: http://community.elgg.org/pg/profile/arsalanlee/
+ * @package Liang Lee Rightclick Plugin
+ * @author Liang Lee
+ * @copyright All right reserved Liang Lee 2012.
+ * @File Version.php
+ */
+$LiangLeeRightclick_version = '20120531';
+$LiangLeeRightclick_release = '1.0.0
+';
+?>
